9b949ff73971fcc6cd975b55b1d2024c04aa0cbb
Liam Tyrie <liamtyrie@gmail.com>
2024-12-19 10:50:06 +0000

    Start on commenting the code so far

--- common-kafka/src/lib.rs
+++ common-kafka/src/lib.rs
@@ -10,6 +10,13 @@ use common_error::error::KafkaResult;
 
 use async_trait::async_trait;
 
+/// Simple placeholder struct, that allows you to create a heap-allocated instance of MessagePrinter wrapped in a Box.
+/// This has been done this way so that the object has a stable memory address, and the ownership and allocation are
+/// flexible
+///
+/// Using a box isn't completely necessary here, but if we are going to be storing large data later on or using
+/// a polymorphic hierarchy
+
 pub struct MessagePrinter {}
 
 impl MessagePrinter {
@@ -18,6 +25,10 @@ impl MessagePrinter {
     }
 }
 
+/// Rust doesn't support async functions directly within a trait definition, due to lifetime constraints within the language.
+/// The async_trait macro works around this. It will rewrite the trait and its impl in a way that makes it work with async methods.
+///
+/// the handle method has 2 params; key and payload. Represented as a byte slice
 #[async_trait]
 impl MessageHandler for MessagePrinter {
     async fn handle(&self, key: &[u8], payload: &[u8]) -> KafkaResult<()> {
@@ -28,16 +39,25 @@ impl MessageHandler for MessagePrinter {
     }
 }
 
+/// This is the main function that processes the Kafka Messages concurrently.
 #[tokio::main]
 async fn main() -> KafkaResult<()> {
+    // Initialise the tracing library for structured logging.
+
     tracing_subscriber::fmt::init();
+
+    // inbound_config: initiates a new config for the inbound consumer.
+    // fulfillment_config: initiates a new config for the fulfillment consumer.
 
     let inbound_config = InboundConfig::new();
     let fulfillment_config = FulfillmentConfig::new();
 
+    // inbound_consumer: creates a new consumer for the Inbound Pipeline
+    // fulfillment_consumer: creates a new consumer for the Fulfillment Pipeline.
     let inbound_consumer = EventConsumer::new(inbound_config, MessagePrinter::new())?;
     let fulfillment_consumer = EventConsumer::new(fulfillment_config, MessagePrinter::new())?;
 
+    // try_join! executes multiple async tasks and waits for all of them to complete, if any ask returns an error, it stops and propagates the error to the caller
     tokio::try_join!(inbound_consumer.start(), fulfillment_consumer.start())?;
 
     Ok(())
